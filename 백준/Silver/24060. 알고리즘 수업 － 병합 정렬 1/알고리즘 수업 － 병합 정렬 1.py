3dc173839114a10102a7002329a69653a78328c3
cookooyas <119028186+cookooyas@users.noreply.github.com>
2024-03-07 14:47:59 +0900

    [Silver III] Title: 알고리즘 수업 - 병합 정렬 1, Time: 2124 ms, Memory: 142996 KB -BaekjoonHub

---
+++ 백준/Silver/24060. 알고리즘 수업 － 병합 정렬 1/알고리즘 수업 － 병합 정렬 1.py
@@ -0,0 +1,38 @@
+import sys
+def merge_sort(arr):
+    global ans
+    if len(arr) <= 1:
+        return arr
+    mid = (len(arr) + 1) // 2
+    low_arr = merge_sort(arr[:mid])
+    high_arr = merge_sort(arr[mid:])
+
+    sorted_arr = []
+    l = h = 0
+    while l < len(low_arr) and h < len(high_arr):
+        if low_arr[l] <= high_arr[h]:
+            sorted_arr.append(low_arr[l])
+            ans.append(low_arr[l])
+            l += 1
+        else:
+            sorted_arr.append(high_arr[h])
+            ans.append(high_arr[h])
+            h += 1
+
+    while l < len(low_arr):
+        sorted_arr.append(low_arr[l])
+        ans.append(low_arr[l])
+        l += 1
+
+    while h < len(high_arr):
+        sorted_arr.append(high_arr[h])
+        ans.append(high_arr[h])
+        h += 1
+
+    return sorted_arr
+
+n,k=map(int,sys.stdin.readline().split())
+l=list(map(int,sys.stdin.readline().split()))
+ans=[]
+merge_sort(l)
+print(ans[k-1] if k<=len(ans) else -1)
